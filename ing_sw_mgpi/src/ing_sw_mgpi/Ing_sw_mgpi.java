159e7ca52f696b14c47e975e28641e9d23d8b70a
Dieorodriguezi <52377564+Dieorodriguezi@users.noreply.github.com>
2019-06-30 09:16:08 -0500

    Update Ing_sw_mgpi.java

--- ing_sw_mgpi/src/ing_sw_mgpi/Ing_sw_mgpi.java
+++ ing_sw_mgpi/src/ing_sw_mgpi/Ing_sw_mgpi.java
@@ -15,7 +15,7 @@ public class Ing_sw_mgpi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Sisystem.out.Printl("Hola Mundo");
     }
     
 }
